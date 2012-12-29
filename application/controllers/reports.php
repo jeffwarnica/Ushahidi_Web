2d39e6af804e76066a2aba2d4b25100e555d2a07
Jeff Warnica <jeff@coherentnetworksolutions.com>
2012-12-28 21:56:30 -0400

    Signed-off-by: Jeff Warnica <jeff@coherentnetworksolutions.com>

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -1005,8 +1005,10 @@ class Reports_Controller extends Main_Controller {
 	
 		error_log(__FILE__ . "#switch_category() \$category_id: $category_id, \$incident_id: $incident_id");
 
-		$category = Category_Model::categories($category_id);
+		$category = Category_Model::categories($category_id);		
 		$form_id = $category['form_id'];
+		
+		error_log(__FILE__ . "#switch_category() \$category_id: $category_id, \$incident_id: $incident_id.\t\t Default \$form_id: $form_id");
 		
 		$form_fields = customforms::switcheroo($incident_id,$form_id);
 		echo json_encode(array("status"=>"success", "response"=>$form_fields));
